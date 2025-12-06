df017c166c0f276d29acd35afa54e6ec4a3077f5
n1koXu <dw981124@gmail.com>
2025-12-06 14:47:50 +0800

    Add files via upload

---
+++ js/css.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary: '#3B82F6',
+                secondary: '#10B981',
+                danger: '#EF4444',
+                neutral: '#64748B',
+                light: '#F8FAFC',
+                dark: '#1E293B'
+            },
+            fontFamily: {
+                sans: ['Inter', 'system-ui', 'sans-serif'],
+            },
+        }
+    }
+}
